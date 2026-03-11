3c5c9f41786ec71b3ef21bf8fb1a6bcdf99907bf
aA-Set-sun <343303426@tdsb.ca>
2026-03-11 16:00:22 +0000

    agh

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,47 @@
+/* Title: More Functions 
+	Name: Angie
+	Date created: March 11, 2026
+	Date updated: March 11, 2026 
+	*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+	}
 
+	
+	public static boolean isEven(int num) {
+			
+		boolean even = num % 2 == 0;
+		
+		return even;
+		}
+
+	public static boolean isOdd(int num) {
+
+		boolean odd = num % 2 != 0;
+		return odd;
+
+	}
+
+	public static boolean isPositive(int num) {
+
+		boolean posit = num > 0;
+		return posit; 
+
+	}
+
+	public static boolean isNegative(int num) {
+
+		boolean negati = num < 0;
+		return negati;
+
+	}
+
+	public static String combinedLength(String word, String word2) {
+
+		String combinedWord = word + word2;
+		return combinedWord;
 	}
 
 }
